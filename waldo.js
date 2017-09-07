535084e1c3fdebbd2732797cbac0c76aea3c0a5a
zoeyyandi <zoeyyandi@gmail.com>
2017-09-07 16:28:05 +0000

    new changes

--- waldo.js
+++ waldo.js
@@ -1,5 +1,6 @@
+
 // The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
+var findWaldo = function(arr, found) {
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
       found(i);   // execute callback
@@ -8,8 +9,26 @@ function findWaldo(arr, found) {
 }
 
 function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index + "!");
+}
 
-  console.log(`Found Waldo at index ${index}!`);
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+
+
+// Replaced for loop with forEach function
+var findWaldo = function(arr, found) {
+  arr.forEach(function(name, index) {
+    if (name === 'Waldo') {
+      found(index)
+    }
+  })
+}
+
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index + "!");
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
